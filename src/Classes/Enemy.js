e6b45cb6e7a9964e63224661db8d46e9fb28f158
matyas94 <marton.matyas@students.jedlik.eu>
2026-02-17 12:18:53 +0100

    enemy health display and sword damage implementation

--- src/Classes/Enemy.js
+++ src/Classes/Enemy.js
@@ -12,11 +12,31 @@ export class Enemy{
 
     draw(ctx){
         if (!this.dead){
-        ctx.drawImage(
-            this.image,
-            this.x,
-            this.y
-        )} else {
+            ctx.drawImage(
+                this.image,
+                this.x,
+                this.y)
+            ctx.textAlign = "center"
+            ctx.font = "bold 20px serif"
+            switch(this.health){
+                case 5: ctx.fillStyle = "#00FF00"
+                break;
+                case 4: ctx.fillStyle = "#9ACD32"
+                break;
+                case 3: ctx.fillStyle = "#FFFF00"
+                break;
+                case 2: ctx.fillStyle = "#FFA500"
+                break;
+                case 1: ctx.fillStyle = "#FF0000"
+                break;
+            }
+            
+            ctx.fillText(
+                this.health,
+                this.x + this.tilesize *0.5,
+                this.y - this.tilesize *0.1
+            )
+            } else {
             this.x = -100
             this.y = -100
         }
@@ -25,38 +45,52 @@ export class Enemy{
     update(player, sword, map){
         if (this.active){
             if (player.x < this.x){
-                this.x -= this.tilesize
+                const targetX = this.x - this.tilesize;
+                const targetY = this.y;
+                if (!map.isWall(targetX, targetY)) {
+                    this.x -= this.tilesize
+                }
+                
             }
             if (player.x > this.x){
                 const targetX = this.x + this.tilesize;
                 const targetY = this.y;
-                
-                this.x += this.tilesize
+                if (!map.isWall(targetX, targetY)) {
+                    this.x += this.tilesize
+                }
             }
             if (player.y < this.y){
-                this.y -= this.tilesize
+                const targetX = this.x
+                const targetY = this.y - this.tilesize;
+                if (!map.isWall(targetX, targetY)) {
+                    this.y -= this.tilesize
+                }
             }
             if (player.y > this.y){
-                this.y += this.tilesize
+                const targetX = this.x 
+                const targetY = this.y + this.tilesize
+                if (!map.isWall(targetX, targetY)) {
+                    this.y += this.tilesize
+                }
             }
 
             if (sword.x - this.tilesize/2 == this.x){
-                this.health -= 1
+                this.health -= sword.dmg
                 console.log(this.health);
                 
                 }
             if (sword.x + this.tilesize/2 == this.x){
-                this.health -= 1
+                this.health -= sword.dmg
                 console.log(this.health);
                 
                 }
             if (sword.y - this.tilesize/2 == this.x){
-                this.health -= 1
+                this.health -= sword.dmg
                 console.log(this.health);
                 
                 }
             if (sword.y + this.tilesize/2 == this.x){
-                this.health -= 1
+                this.health -= sword.dmg
                 console.log(this.health);
                 
                 }
